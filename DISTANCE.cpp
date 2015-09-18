fb5a8989c20913fc0e413549a217432224bf85ca
mofr <mofr@zond.org>
2015-09-18 18:44:21 +0600

    Optimize DISTANCE

--- DISTANCE.cpp
+++ DISTANCE.cpp
@@ -31,6 +31,21 @@ int main()
 	int * points = new int[size];
 
 	int extremums[1 << MAX_DIMENSIONS];	
+	int extremumCount = 1 << d;
+
+	int signs[extremumCount * d];
+	int s = 0;
+	for(int e = 0; e < extremumCount; ++e)
+	{
+		for(int k = 0; k < d; ++k)
+		{
+			signs[s] = ((e >> k) % 2 ? 1 : -1);
+			++s;
+		}
+	}
+
+
+//	std::cout << "Init done, elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << " ms" << std::endl;
 
 	std::string input;
 	static const int BUFFER_SIZE = 1024*1024;
@@ -52,12 +67,14 @@ int main()
 			points[out+k] = strtol(inputStr, &inputStr, 10);
 		}
 		bool notgood = true;
-		for(int e = 0; e < (1<<d); ++e)
+		int s = 0;
+		for(int e = 0; e < extremumCount; ++e)
 		{
 			int extremum = 0;
 			for(int k = 0; k < d; ++k)
 			{
-				extremum += points[out+k] * ((e >> k) % 2 ? 1 : -1);
+				extremum += points[out+k]*signs[s];
+				++s;
 			}
 			if(i == 0 || extremum >= extremums[e])
 			{
